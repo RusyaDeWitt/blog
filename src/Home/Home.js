d783960f66fe31c0cec27949557c5e213d336d18
RusyaDeWitt <kashayevruslan@gmail.com>
2022-02-18 20:34:59 +0600

    changes

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -15,38 +15,39 @@ function Home(){
     <img src="./wallpaper.gif" className="img-fluid"/>
 
 
-    <Container className="about">
-    <h1 >About me</h1>
-      <p>
-      Hey! I am Ruslan, but you can call me Rusya if you want. I am student of Kazakh-British Technical University. Honestly,
-      I was never best at school or academics, I am just super curious guy. A lot of stuff interests me, one of them is programming.
-      I have a lot of ideas and here I will post my progress
-      </p>
 
-      <div className="lists">
-      <p id="tmp"> My Stack of Techonologies </p>
-          <ul>
-            <li> React-bootstrap </li>
-            <li> JavaScript </li>
-            <li> HTML/CSS </li>
-            <li> PostgreSQL </li>
-            <li> Python </li>
-          </ul>
-      </div>
+        <h1 className="text">My work experience</h1>
 
-      <div className="lists">
-      <p id="tmp"> Languages </p>
-          <ul>
-            <li> Kazakh (C1) </li>
-            <li> Russian (C2) </li>
-            <li> English (B2) </li>
-          </ul>
-      </div>
-    </Container>
+        <Cards />
 
-      <h1 className="text">My work experience</h1>
+        <div className="about">
+        <h1>About me</h1>
+          <p>
+          Hey! I am Ruslan, but you can call me Rusya if you want. I am student of Kazakh-British Technical University. Honestly,
+          I was never best at school or academics, I am just super curious guy. A lot of stuff interests me, one of them is programming.
+          I have a lot of ideas and here I will post my progress
+          </p>
 
-      <Cards />
+          <div className="lists">
+          <p id="tmp"> My Stack of Techonologies </p>
+              <ul>
+                <li> React-bootstrap </li>
+                <li> JavaScript </li>
+                <li> HTML/CSS </li>
+                <li> PostgreSQL </li>
+                <li> Python </li>
+              </ul>
+          </div>
+
+          <div className="lists">
+          <p id="tmp"> Languages </p>
+              <ul>
+                <li> Kazakh (C1) </li>
+                <li> Russian (C2) </li>
+                <li> English (B2) </li>
+              </ul>
+          </div>
+        </div>
 
 
       <div className="footer">
